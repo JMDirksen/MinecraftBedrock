1759c70b540b2fa27faec996cf528b8549799ab8
JMDirksen <jeftadirksen@gmail.com>
2022-12-24 12:36:11 +0100

    Restarting message

--- restart.sh
+++ restart.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 echo "Counting down... (30 sec.)"
-screen -S "minecraftbedrock" -p 0 -X stuff "say Server is restarting in 30 seconds^M"
+screen -S "minecraftbedrock" -p 0 -X stuff "say Restarting in 30 seconds^M"
 sleep 20
-screen -S "minecraftbedrock" -p 0 -X stuff "say Server is restarting in 10 seconds^M"
+screen -S "minecraftbedrock" -p 0 -X stuff "say Restarting in 10 seconds^M"
 sleep 5
-screen -S "minecraftbedrock" -p 0 -X stuff "say Server is restarting in 5...^M"
+screen -S "minecraftbedrock" -p 0 -X stuff "say Restarting in 5...^M"
 sleep 2
-screen -S "minecraftbedrock" -p 0 -X stuff "say Server is restarting in 3...^M"
+screen -S "minecraftbedrock" -p 0 -X stuff "say Restarting in 3...^M"
 sleep 1
-screen -S "minecraftbedrock" -p 0 -X stuff "say Server is restarting in 2...^M"
+screen -S "minecraftbedrock" -p 0 -X stuff "say Restarting in 2...^M"
 sleep 1
-screen -S "minecraftbedrock" -p 0 -X stuff "say Server is restarting in 1...^M"
+screen -S "minecraftbedrock" -p 0 -X stuff "say Restarting in 1...^M"
 sleep 1
 screen -S "minecraftbedrock" -p 0 -X stuff "stop^M"
 echo "Restarting!"
