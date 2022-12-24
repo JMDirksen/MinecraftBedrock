21dc31d04f619413b804d8a43d9546fb3cdaa7c4
JMDirksen <jeftadirksen@gmail.com>
2022-12-24 16:24:26 +0100

    Keep only 1 day of backups

--- backup.sh
+++ backup.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 # Create backups directory
 [ ! -d backups ] && mkdir backups
 # Remove old backups
-find backups/backup-*.tar.gz -mtime +28 -type f -delete
+find backups/backup-*.tar.gz -mtime +1 -type f -delete
 # Create backup
 tar -czf backups/backup-$(date '+%Y%m%d%H%M%S').tar.gz server/worlds server/server.properties server/permissions.json
