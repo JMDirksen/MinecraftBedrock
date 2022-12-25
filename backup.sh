a2bbcd0eb002a566c7142960de5d82de8362f0d7
JMDirksen <jeftadirksen@gmail.com>
2022-12-25 09:26:39 +0100

    Server logging

--- backup.sh
+++ backup.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 # Remove old backups
 find backups/backup-*.tar.gz -mtime +1 -type f -delete
 # Create backup
-tar -czf backups/backup-$(date '+%Y%m%d%H%M%S').tar.gz server/worlds server/server.properties server/permissions.json
+tar -czf backups/backup-$(date '+%y%m%d%H%M').tar.gz server/worlds server/server.properties server/permissions.json logs
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-screen -dmS "minecraftbedrock" "./loop.sh"
+[ ! -d logs ] && mkdir logs
+find logs/server-*.log -mtime +30 -type f -delete
+screen -dmS "minecraftbedrock" -L -Logfile logs/server-$(date '+%y%m%d%H%M').log "./loop.sh"
