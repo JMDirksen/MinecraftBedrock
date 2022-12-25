0d8f72273b0a975b7a725f15d45d3a17a5dc9d56
JMDirksen <jeftadirksen@gmail.com>
2022-12-25 09:52:23 +0100

    Strings

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "$0")"
 
 # Create backups directory
-[ ! -d backups ] && mkdir backups
+[ ! -d "backups" ] && mkdir "backups"
 # Remove old backups
-find backups/backup-*.tar.gz -mtime +1 -type f -delete
+find "backups/" -mtime +1 -type f -delete
 # Create backup
-tar -czf backups/backup-$(date '+%y%m%d%H%M').tar.gz server/worlds server/server.properties server/permissions.json logs
+tar -czf "backups/backup-$(date '+%y%m%d%H%M').tar.gz" "server/worlds" "server/server.properties" "server/permissions.json" "logs"
--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-[ ! -d logs ] && mkdir logs
-find logs/server-*.log -mtime +30 -type f -delete
-screen -dmS "minecraftbedrock" -L -Logfile logs/server-$(date '+%y%m%d%H%M').log "./loop.sh"
+
+# Create logs directory
+[ ! -d "logs" ] && mkdir "logs"
+# Cleanup logs
+find "logs/" -mtime +30 -type f -delete
+# Start server loop in screen with logging
+screen -dmS "minecraftbedrock" -L -Logfile "logs/server-$(date '+%y%m%d%H%M').log" "./loop.sh"
