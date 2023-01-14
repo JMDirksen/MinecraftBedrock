298dae434ba827497154da77b7fb9d48b0badf1a
JMDirksen <jeftadirksen@gmail.com>
2023-01-14 15:38:04 +0100

    Store less backups

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 # Create backups directory
 [ ! -d "backups" ] && mkdir "backups"
 # Remove old backups
-find "backups/" -mtime +7 -type f -delete
+find "backups/" -mtime +1 -type f -delete
 # Create backup
 backupfile="backups/backup-$(date '+%y%m%d%H%M').tar.gz"
 tar -czf "$backupfile" "server" "logs"
